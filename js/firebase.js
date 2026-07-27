c5fa1e7a0c9bd0be2f6a3226f25b7e3e583da073
JoshuaChew12 <jayz.chew.one@gmail.com>
2026-07-27 17:43:07 +0800

    Add Firebase Realtime Counter functionality

---
+++ js/firebase.js
@@ -0,0 +1,26 @@
+// =====================================
+// FIREBASE REALTIME COUNTER
+// =====================================
+firebase.initializeApp({
+
+databaseURL:
+"https://nga-worship-default-rtdb.asia-southeast1.firebasedatabase.app/"
+
+});
+
+const firebaseDB=firebase.database();
+
+function startFirebaseCounter(){
+
+firebaseDB
+.ref("count")
+.on("value",snap=>{
+
+set(
+"checkCount",
+snap.val()||0
+);
+
+});
+
+}
